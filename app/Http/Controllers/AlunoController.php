50cdf711f5590ef1b75074d929bb9f21cd0735b5
Gaby <1gabycamargo@gmail.com>
2026-09-19 12:49:31 -0300

    ATV 4 - Implementa metodos CRUD

--- app/Http/Controllers/AlunoController.php
+++ app/Http/Controllers/AlunoController.php
@@ -6,5 +6,38 @@ use Illuminate\Http\Request;
 
 class AlunoController extends Controller
 {
-    //
+    public function index()
+    {
+        return 'Lista de alunos';
+    }
+
+    public function show($id)
+    {
+        return 'Aluno: ' . $id;
+    }
+
+    public function create()
+    {
+        return 'Formulário de cadastro';
+    }
+
+    public function store(Request $request)
+    {
+        return 'Aluno cadastrado';
+    }
+
+    public function edit($id)
+    {
+        return 'Editar aluno: ' . $id;
+    }
+
+    public function update(Request $request, $id)
+    {
+        return 'Aluno atualizado: ' . $id;
+    }
+
+    public function destroy($id)
+    {
+        return 'Aluno excluído: ' . $id;
+    }
 }
